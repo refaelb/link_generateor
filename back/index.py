d92dbe419e733938d7cee4d306f272660af87a3c
Your Name <you@example.com>
2022-10-27 08:26:48 +0300

    add function to read from db

--- back/index.py
+++ back/index.py
@@ -5,6 +5,8 @@ import requests
 from flask import render_template
 from flask_cors import CORS
 import pymongo
+from datetime import datetime
+
 
 
 
@@ -25,30 +27,57 @@ def pipline_api():
     return a
 
 
+
 def validate(data):
     json_data=json.loads(data)
     print (json_data)
+    token_user = int(json_data["token_user"])
+    amount = int(json_data["amount"])
     count_links = int(json_data["count_links"])
-    count = int(json_data["count"])
-    id_user = int(json_data["id_user"])
-    out = []
-    for i in range (count):
-        out.append(api_createor(count, count_links, id_user))
-    print (out)
-    return out
+    
+    return read_from_mongo(token_user, amount, count_links)
 
-def api_createor(count, count_links, id_user):
+def read_from_mongo(token_user, amount, count_links):
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["mydatabase"]
+    mycol = mydb["links"]
+    myquery = { "name": token_user }
+    # mydoc = mycol.find().sort(token_user, -1).limit(1)
+    # # print (mydoc)
+    # count_orders=mydoc["count_orders"]
+    mydoc = mycol.find().sort("count_orders", -1)
+    # print (mydoc["name"])
+    for x in mydoc:
+    #   print(x["name"])
+        if x["name"] == token_user:
+            count_orders=x["count_orders"]
+            print (count_orders)
+            break
+        else:
+            count_orders=0
+    print (count_orders)
+    count_orders=count_orders+1
+    out = []
+    for i in range (count_links):
+        out.append(api_createor(token_user, amount, count_links))
+    print (out)
+    write_to_mongo(token_user, amount, count_links, out, count_orders)
+    return (out)
+
+
+
+def api_createor(token_user, amount, count_links):
     url = "https://stoplight.io/mocks/coinpay/api-2-0/59787578/api/integration/link/createLink/v1"
 
     payload = json.dumps({
     "CurrencyCode": "BTC",
     "IdCurrencyPay": 6,
-    "Amount": count,
-    "IdReference": id_user,
+    "Amount": amount,
+    "IdReference": token_user,
     "Address": "1AkfP72SZiM1zxwkk5GttXrLi7mmTAmFmk"
     })
     headers = {
-    'Authorization': 'Bearer Bearer 42c1823c7c014de7e19d6d8ca76a1aa43e08d1a5bd420baa1ca271f3bb486e64',
+    'Authorization': 'Bearer  42c1823c7c014de7e19d6d8ca76a1aa43e08d1a5bd420baa1ca271f3bb486e64',
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
     'Cookie': 'GCLB=CPqyg5CBhdrDyQE; __cf_bm=P0iGBOYWMhbvIZfWmdIhESTSvzmrlHkCmW70NdDZFTM-1666557847-0-AZ9hUutXbZFk/t5lZ7qJEefSVbiTlpQTXEp8lS7lnNeNUbbFLh42SXVA0VebZzIyWuv0Us/vtyvwsduliBq/2Dc='
@@ -58,13 +87,17 @@ def api_createor(count, count_links, id_user):
     print(response.content)
     json_data=json.loads(response.content)
     links=json_data['Data']
-    # return links
-    return write_to_mongo(count, count_links, id_user, links)
+    return links
 
-def write_to_mongo(count, count_links, id_user,links):
+def write_to_mongo(token_user, amount, count_links,links, count_orders):
+    dt = datetime.now()
+    ts = datetime.timestamp(dt)
+    print("Date and time is:", dt)
+
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     mydb = myclient["mydatabase"]
     mycol = mydb["links"]
-    mydict = { "name": id_user, "links": links , "count_orders": 3, "amount": count}
+    mydict = { "name": token_user, "links": links , "count_orders": count_orders, "amount": amount, "time": dt }
 
     x = mycol.insert_one(mydict)
+    return(links)
--- back/mongo.py
+++ back/mongo.py
@@ -1,8 +1,17 @@
+from unicodedata import name
 import pymongo
+token_user=34
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
-mycol = mydb["customers"]
+mycol = mydb["links"]
 
-mydict = { "name": "John", "address": "Highway 37" }
+mydoc = mycol.find().sort("count_orders", -1)
+# print (mydoc["name"])
+for x in mydoc:
+#   print(x["name"])
+  if x["name"] == token_user:
+    count_orders=x["count_orders"]
+    print (count_orders)
+    break
 
-x = mycol.insert_one(mydict)
+
